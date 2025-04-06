19f7ed437e765b103b2c1d1c6fda0dd738235093
jacopovalanzano <jacopo.valanzano@gmail.com>
2025-04-06 22:19:57 +0100

    New test RequestTest.php

--- tests/Http/RequestTest.php
+++ tests/Http/RequestTest.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 use PHPUnit\Framework\TestCase;
+use Tundra\Http\Request;
 
 class RequestTest extends TestCase
 {
@@ -18,7 +19,7 @@ class RequestTest extends TestCase
     {
         $_SERVER["REQUEST_URI"] = "/";
 
-        $request = new \Tundra\Http\Request();
+        $request = new Request();
         $this->assertTrue($request->capture() instanceof \Tundra\Http\Request);
     }
 
@@ -27,7 +28,7 @@ class RequestTest extends TestCase
      */
     public function testCreateFromBase()
     {
-        $request = new \Tundra\Http\Request();
+        $request = new Request();
         $this->assertTrue($request->createFromBase(new \Symfony\Component\HttpFoundation\Request(
                 array(), array(), array(), array(), array(), array()
             )) instanceof \Tundra\Http\Request);
@@ -38,7 +39,7 @@ class RequestTest extends TestCase
      */
     public function testInitialize()
     {
-        $request = new \Tundra\Http\Request();
+        $request = new Request();
 
         $this->assertEmpty($request->request);
         $this->assertEmpty($request->query);
@@ -48,7 +49,14 @@ class RequestTest extends TestCase
         $this->assertEmpty($request->server);
         $this->assertEmpty($request->headers);
 
-        $request->initialize(array(), array(), array(), array(), array(), array());
+        $request->initialize(
+            $_GET,
+            $_POST,
+            $_REQUEST,
+            $_COOKIE,
+            $_FILES,
+            $_SERVER
+        );
 
         $this->assertTrue($request->request instanceof \Symfony\Component\HttpFoundation\InputBag);
         $this->assertTrue($request->query instanceof \Symfony\Component\HttpFoundation\InputBag);
@@ -64,8 +72,61 @@ class RequestTest extends TestCase
      */
     public function testGetMethod()
     {
-        $request = new \Tundra\Http\Request();
+        $request = new Request(
+            $_GET,
+            $_POST,
+            $_REQUEST,
+            $_COOKIE,
+            $_FILES,
+            $_SERVER
+        );
         $this->assertEquals("GET", $request->getMethod());
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetMethodWithOverride()
+    {
+        // Simulate a DELETE request with a method override
+        $request = Request::create(
+            '/path',
+            'POST',
+            [],
+            [],
+            [],
+            ['REQUEST_METHOD' => 'POST'],
+            ''
+        );
+
+        $request->headers->set('X-HTTP-METHOD-OVERRIDE', 'DELETE');
+
+        // Assert that the method is correctly overridden to DELETE
+        $this->assertEquals('DELETE', $request->getMethod());
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetMethodWithPostMethod()
+    {
+        // Simulate a POST request
+        $request = Request::create('/path', 'POST');
+
+        // Assert that the method is POST
+        $this->assertEquals('POST', $request->getMethod());
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetMethodWithGetMethod()
+    {
+        // Simulate a GET request with REQUEST_METHOD being GET
+        $request = Request::create('/path', 'GET');
+
+        // Assert that the method is GET
+        $this->assertEquals('GET', $request->getMethod());
     }
 
     /**
@@ -74,10 +135,10 @@ class RequestTest extends TestCase
     public function testDeleteWithKey()
     {
         $query = ['key' => 'value']; // For example, parameters sent with the URL like /path?key=value
-        $request = []; // No POST data here since DELETE generally doesn’t use it
-        $attributes = []; // You can use this if needed for route parameters
-        $cookies = []; // Simulate cookies, if necessary
-        $files = []; // Simulate files if necessary
+        $request = $_POST;
+        $attributes = $_REQUEST;
+        $cookies = $_COOKIE;
+        $files = $_FILES;
         $server = [
             'REQUEST_METHOD' => 'DELETE', // This is important for simulating the DELETE request
             'HTTP_HOST' => 'localhost', // Typically the host
@@ -88,11 +149,31 @@ class RequestTest extends TestCase
         $content = http_build_query(['key' => 'value']); // Simulate the content of the DELETE request
 
         // Create a new Request object for the DELETE request
-        $requestObject = new \Tundra\Http\Request($query, $request, $attributes, $cookies, $files, $server, $content);
-        
+        $requestObject = new Request($query, $request, $attributes, $cookies, $files, $server, $content);
+
         // Use the $requestObject to get query or content data
         $this->assertEquals('value', $requestObject->query->get('key')); // Test query data
         $this->assertEquals('key=value', $requestObject->getContent()); // Test content data
+    }
 
+    /**
+     * @return void
+     */
+    public function testGetProtocolVersion()
+    {
+        if(! isset($_SERVER["SERVER_PROTOCOL"])) {
+            $_SERVER["SERVER_PROTOCOL"] = "HTTP/1.1";
+        }
+
+        $request = new Request(
+            $_GET,
+            $_POST,
+            $_REQUEST,
+            $_COOKIE,
+            $_FILES,
+            $_SERVER
+        );
+
+        $this->assertEquals("HTTP/1.1", $request->getProtocolVersion());
     }
 }
