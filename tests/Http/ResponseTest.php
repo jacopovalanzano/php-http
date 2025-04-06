0f15e1b93ed17563440cc21dc2ff415fc7bd0a2d
jacopovalanzano <jacopo.valanzano@gmail.com>
2025-04-06 22:20:16 +0100

    New test ResponseTest.php

--- tests/Http/ResponseTest.php
+++ tests/Http/ResponseTest.php
@@ -7,11 +7,25 @@ use PHPUnit\Framework\TestCase;
 
 class ResponseTest extends TestCase
 {
+    /**
+     * @return void
+     */
     public function testResponse()
     {
         $response = new \Tundra\Http\Response("Hello World", 200, ["Content-Type" => "text/plain"]);
         $this->assertEquals("Hello World", $response->getContent());
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals("text/plain", $response->headers->get("Content-Type"));
+    }
+
+    /**
+     * @return void
+     */
+    public function testSetContent()
+    {
+        $response = new \Tundra\Http\Response();
+        $this->assertEquals("Hello World", $response->setContent("Hello World")->getContent());
+        $this->assertEquals('["Hello World"]', $response->setContent(["Hello World"])->getContent()); // Test array
+        $this->assertEquals('["Hello World"]', $response->setContent('["Hello World"]')->getContent()); // Test json
     }
 }
